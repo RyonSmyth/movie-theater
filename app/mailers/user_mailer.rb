0f568a0406d287c5f8842bbcb8e0549b057b641d
RyonSmyth <ryans970@hotmail.com>
2015-07-23 00:26:20 -0700

    Fixed Contact Form

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,13 +2,15 @@ class UserMailer < ApplicationMailer
 
 	def contact_form(email, name, message)
 	  @message = message
-	  mail(:from => email, :to => 'ryans970@homtail.com', :subject "SoCal Theater Message from #{name}"),
+	  mail(:from => email,
+	   :to => 'ryans970@homtail.com',
+	   :subject => "SoCal Theater Message from #{name}")
 	end
 
 	def thank_you
 		@name = params[:name]
 		@email = params[:email]
 		@message = params[:message]
-		UserMailer.thank_you(@email, @name, @message).deliver
+		UserMailer.contact_form(@email, @name, @message).deliver
 	end
 end
